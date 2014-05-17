8e1f0de941c07715bc522b5c2ff7140fa5ab4765
vakata <git@vakata.com>
2014-05-17 15:11:18 +0300

    better error handling back to single token resizable + per repo width

--- src/inject.js
+++ src/inject.js
@@ -1,11 +1,4 @@
-// clean dependencies
-// clean CSS
-// make resizable
-// clean the template
-// gists (gist.github.com)
-// header height & styles + ellipsis
 // fix for translate
-// move button inside
 
 (function($, undefined) {
 	$(document).ready(function() {
@@ -15,9 +8,9 @@
 			temp = [ null, null, null, null, null ],
 			tree = null,
 			html = null,
-			tokn = null,
+			tokn = window.localStorage.getItem('octotree.token'),
 			detectLocationChange = function() {
-				if (location.href !== href || location.hash != hash) {
+				if (!repo[1] || location.href !== href || location.hash != hash) {
 					href = location.href;
 					hash = location.hash;
 					temp = location.pathname.match(/([^\/]+)\/([^\/]+)(?:\/([^\/]+))?/);
@@ -33,8 +26,8 @@
 						temp[4] = $('*[data-master-branch]').data('ref') || 'master';
 						if(repo[1] !== temp[1] || repo[2] !== temp[2] || repo[4] !== temp[4]) {
 							repo = temp.slice();
-							tokn = window.localStorage.getItem('octotree.token.' + repo[1] + '.' + repo[2]);
 							html[window.localStorage.getItem('octotree.visible.' + repo[1] + '.' + repo[2]) ? 'addClass' : 'removeClass']('octotree-visible');
+							html.width(window.localStorage.getItem('octotree.width.' + repo[1] + '.' + repo[2]) || 250);
 							html.find('h1').text(repo[1] + ' / ' + repo[2]).end().find('h2').text(repo[4]);
 							tree.settings.state.key = 'octotree.' + repo[1] + '.' + repo[2];
 							tree.deselect_all();
@@ -61,19 +54,24 @@
 					'</hgroup>' +
 					'<form class="octotree-form">' +
 						'<p class="octotree-message"></p>' +
-						'<input name="token" type="text" placeholder="Paste access token here" value=""></input>' +
+						'<input name="token" type="text" placeholder="Paste access token here" value="'+tokn+'"></input>' +
 						'<button type="submit" class="button">Save</button>' +
 					'</form>' +
 					'<div class="octotree-tree"></div>' +
 					'<a class="octotree-toggle button"><span></span></a>' +
+					'<div class="octotree-resizer"></div>' +
 				'</nav>')
+				.on('mousedown', '.octotree-resizer', function (e) {
+					return $.vakata.dnd.start(e, { 'octotree' : true, 'x' : e.pageX, 'w' : parseInt(html.width(),10) }, false);
+				})
 				.on('click', '.octotree-toggle', function () {
 					$(this).closest('.octotree-sidebar').toggleClass('octotree-visible');
 					window.localStorage.setItem('octotree.visible.' + repo[1] + '.' + repo[2], !window.localStorage.getItem('octotree.visible.' + repo[1] + '.' + repo[2]));
 				})
 				.on('submit', '.octotree-form', function (e) {
 					e.preventDefault();
-					window.localStorage.setItem('octotree.token.' + repo[1] + '.' + repo[2], $(this).find('input').val());
+					tokn = $(this).find('input').val();
+					window.localStorage.setItem('octotree.token', tokn);
 					$(this).find('.octotree-message').text('').end().hide();
 					$(window).resize();
 					repo = null;
@@ -160,9 +158,18 @@
 					}).end()
 				.appendTo('body');
 		tree = $('.octotree-tree').jstree(true);
-		$(window).on('resize', function () {
-			html.children('.octotree-tree').outerHeight($(window).height() - (html.children('.octotree-header').outerHeight() + (html.children('.octotree-form').is(':visible') ? html.children('.octotree-form').outerHeight() : 0)));
-		}).resize();
+		$(window)
+			.on('resize', function () {
+				html.children('.octotree-tree').outerHeight($(window).height() - (html.children('.octotree-header').outerHeight() + (html.children('.octotree-form').is(':visible') ? html.children('.octotree-form').outerHeight() : 0)));
+			})
+			.resize();
+		$(document).bind('dnd_move.vakata', function (e, data) {
+				if(!data.data.octotree) { return; }
+				html.width(data.data.w + (data.event.pageX - data.data.x));
+			})
+			.bind('dnd_stop.vakata', function (e, data) {
+				window.localStorage.setItem('octotree.width.' + repo[1] + '.' + repo[2], html.width());
+			});
 		detectLocationChange();
 	});
 })(jQuery);
